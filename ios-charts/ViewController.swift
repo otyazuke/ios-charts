d15999f68b14173f3f4b713585a9094b015988df
Yuki-Ikegami <fp25657.hawks@icloud.com>
2016-12-04 23:53:48 +0900

    Line Chart Create

--- ios-charts/ViewController.swift
+++ ios-charts/ViewController.swift
@@ -11,7 +11,8 @@ import Charts
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var graphView: BarChartView!
+    @IBOutlet var graphView: LineChartView!
+    
     var manths: [String]!
     
     override func viewDidLoad() {
@@ -20,14 +21,12 @@ class ViewController: UIViewController {
         manths = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
         let unitsSlid = [50.3, 68.3, 113.3, 115.7, 160.8, 214.0, 220.4, 132.1, 176.2, 120.9, 71.3, 48.0]
         
-        graphView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
-        graphView.animate(yAxisDuration: 2.0)
-        graphView.pinchZoomEnabled = false
-        graphView.drawBarShadowEnabled = false
-        graphView.drawBordersEnabled = false
-        graphView.chartDescription?.text = "テストグラフ"
+        self.graphView.chartDescription?.text = "tap node for details"
+        self.graphView.chartDescription?.textColor = UIColor.white
+        self.graphView.gridBackgroundColor = UIColor.darkGray
+        self.graphView.noDataText = "No data provided"
         
-        setChart(dataPoints: manths, values: unitsSlid)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,18 +35,8 @@ class ViewController: UIViewController {
     }
 
     func setChart(dataPoints: [String], values: [Double]) {
-        graphView.noDataText = "You need to provide data for the chart"
         
-        var dataEntries: [BarChartDataEntry] = []
         
-        for i in 0..<dataPoints.count {
-            let dataEntry = BarChartDataEntry(x: Double(i), yValues: [values[i]])
-            dataEntries.append(dataEntry)
-        }
-        
-        let chartDataSet = BarChartDataSet(values: dataEntries, label: "降水量")
-        let chartData = BarChartData(dataSet: chartDataSet)
-        graphView.data = chartData
     }
 
 }
